05e0a02ad02bcb05024bec4a942f2f5e80f5995b
Rahul S <s_rahul@yahoo.com>
2024-05-16 11:14:55 +0200

    deployed on streamlit

--- src/hello_streamlit.py
+++ src/hello_streamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.header("The amazing YouTube video of a cave", anchor="Cave", 
+st.header("An amazing YouTube video of a cave", anchor="Cave", 
           help="There is no one coming to help you in a cave. Help yourself", divider=False)
 st.subheader("(c) Rahul Swaminathan", anchor = None, help = None, divider = True)
 st.text("A really cool cave in Sardegna thats tight, loops under itself \n and beautiful to navigate inside")
